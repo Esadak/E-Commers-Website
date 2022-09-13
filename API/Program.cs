fab885ee901602e778cb696038331ab97aed3cb5
Ezat Akbari <ezatakbari23@hotmail.com>
2022-09-13 18:00:40 +0200

    Added som new features

--- API/Program.cs
+++ API/Program.cs
@@ -6,14 +6,35 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+           var host = CreateHostBuilder(args).Build();
+          using var scope = host.Services.CreateScope();
+          var services = scope.ServiceProvider;
+
+          var logger = services.GetRequiredService<ILogger<Program>>();
+     
+          try{
+            var context = services.GetRequiredService<StoreContext>();
+            await context.Database.MigrateAsync();
+            await StoreContextSeed.SeedAsyn(context, logger);
+
+          }catch(Exception ex)
+          {
+            
+            logger.LogError(ex, "Something wrong happened during migration");
+
+          }
+          await host.RunAsync();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
